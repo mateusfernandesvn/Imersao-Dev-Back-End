20622db60c04da308828f97887c09ec269272842
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-11-21 12:56:27 -0300

    ✅ Aula 4

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,38 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import fs from "fs";
 
 export async function listarPosts(req, res) {
 
     const posts = await getTodosPosts();
-
     res.status(200).json(posts);
 }
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado);
+    } catch (error) {
+        res.status(500).json({ "ERRO": "Falha na requisição" });
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao:"",
+        imagem: req.file.originalnname,
+        alt:""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost)
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado)
+    } catch (erro) {
+        res.status(500).json({ "ERRO": "Falha na requisição" });
+    }
+}
+
 
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,26 @@
-import express from "express";
-import { listarPosts } from "../controllers/postController.js";
+import express from "express"
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ dest: "./uploads", storage })
 
 const routes = (app) => {
-    
+
     app.use(express.json());
+    // Rota para listar todos os posts
     app.get("/posts", listarPosts);
+    // Rota para criar um novo post
+    app.post("/posts", postarNovoPost)
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
