cae2fff242dc402acd8be506c2d838c86e98d314
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-11-22 23:04:51 -0300

    ✅ Aula 5

--- back/src/routes/postRoutes.js
+++ back/src/routes/postRoutes.js
@@ -1,26 +1,38 @@
 import express from "express"
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postController.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postController.js";
+import cors from "cors"
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./uploads");
+        cb(null, "uploads/");
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
     }
 });
 
-const upload = multer({ dest: "./uploads", storage })
+const upload = multer({ dest: "uploads/" , storage});
 
 const routes = (app) => {
 
     app.use(express.json());
+    app.use(cors(corsOptions));
+
     // Rota para listar todos os posts
     app.get("/posts", listarPosts);
+
     // Rota para criar um novo post
     app.post("/posts", postarNovoPost)
+
     app.post("/upload", upload.single("imagem"), uploadImagem)
+
+    app.put("/upload/:id", atualizarNovoPost)
 }
 
 export default routes;
